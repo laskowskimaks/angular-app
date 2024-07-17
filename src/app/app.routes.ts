106fb6f20aa65c325b6125da3f110b87b123cd53
laskowskimaks <laskowski.maksymilian@gmail.com>
2024-07-17 16:31:12 +0200

    poprawienie routingu strony startowej

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     component: HomeComponent,
     title: 'Home', 
